99ec333991a6332c7e412f758fc95c21bd70143a
ANDRE <klai1@mail.ru>
2025-02-18 23:40:53 +0400

    Initial commit

---
+++ src/main/java/com/astontask/astontask/AstonTaskApplication.java
@@ -0,0 +1,13 @@
+package com.astontask.astontask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astontask/astontask/AstonTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astontask.astontask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
