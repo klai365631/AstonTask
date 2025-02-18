cd044a48fbb3df084ccf6b30729528c78edd39e4
ANDRE <klai1@mail.ru>
2025-02-19 00:43:26 +0400

    Done

--- src/main/java/com/astontask/astontask/AstonTaskApplication.java
+++ src/main/java/com/astontask/astontask/AstonTaskApplication.java
@@ -2,9 +2,16 @@ package com.astontask.astontask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AstonTaskApplication {
+public class AstonTaskApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AstonTaskApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AstonTaskApplication.class, args);
